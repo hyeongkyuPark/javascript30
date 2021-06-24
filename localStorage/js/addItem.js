e75ab02c06f8986725fc1417dec0a7b48bd02d1e
hyeongkyuPark <phg0644@gmail.com>
2021-06-24 17:38:05 +0900

    localStorage complete

---
+++ localStorage/js/addItem.js
@@ -0,0 +1,48 @@
+(function () {
+    const addItems = document.querySelector('.add-item');
+    const itemsList = document.querySelector('.plates');
+    const items = JSON.parse(localStorage.getItem('items')) || [];
+
+    const populateList = function (plates = [], platesList) {
+        platesList.innerHTML = plates.map(function (plate, i) {
+            return `
+                <li>
+                    <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : null} />
+                    <label for="item${i}">${plate.text}</label>    
+                </li>
+            `;
+        }).join('');
+    }
+
+    const addItem = function (e) {
+        // 기본 이벤트 발생을 막음(submit의 경우 서버 전송을 막음)
+        e.preventDefault();
+
+        const text = (this.querySelector('[name="item"]')).value;
+        const item = {
+            text,
+            done: false,
+        }
+        items.push(item);
+        populateList(items, itemsList);
+        localStorage.setItem('items', JSON.stringify(items));
+        this.reset();
+    }
+
+    const toggleDone = function (e) {
+        if (!e.target.matches('input')) return; // input가 아니면 미작동 (델리게이션)
+        const el = e.target;
+        const index = el.dataset.index;
+        items[index].done = !items[index].done;
+        localStorage.setItem('items', JSON.stringify(items));
+        populateList(items, itemsList);
+    }
+
+    const init = function () {
+        populateList(items, itemsList);
+        addItems.addEventListener('submit', addItem);
+        itemsList.addEventListener('click', toggleDone);
+    };
+
+    init();
+})();
